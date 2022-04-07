5dc787e8e7a25b1c858f7819703c8baada0bb589
Pranav C <pranavxc@gmail.com>
2022-04-07 21:59:01 +0530

    chore(action): debugging
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- scripts/upgradeNocodbSdk.js
+++ scripts/upgradeNocodbSdk.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path');
-const { mainModule } = require('process');
+const {mainModule} = require('process');
 
 const execSync = require('child_process').execSync;
 
@@ -8,71 +8,73 @@ const execSync = require('child_process').execSync;
 const nocodbSdkPackage = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'packages', 'nocodb-sdk', 'package.json'), 'utf8'))
 
 const replacePackageName = (filePath) => {
-    return new Promise((resolve, reject) => {
-        return fs.readFile(filePath, 'utf8', function (err, data) {
-            if (err) return reject(err)
-            var result = data.replace(/nocodb-sdk/g, nocodbSdkPackage.name);
-            return fs.writeFile(filePath, result, 'utf8', function (err) {
-                if (err) return reject(err)
-                return resolve()
-            });
-        });
-    })
+  return new Promise((resolve, reject) => {
+    return fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) return reject(err)
+      var result = data.replace(/nocodb-sdk/g, nocodbSdkPackage.name);
+      return fs.writeFile(filePath, result, 'utf8', function (err) {
+        if (err) return reject(err)
+        return resolve()
+      });
+    });
+  })
 }
 
 const bumbVersionAndSave = () => {
-    // upgrade nocodb-sdk version in nocodb
-    execSync(`cd packages/nocodb && npm install --save --save-exact ${nocodbSdkPackage.name}@${nocodbSdkPackage.version}`, {});
-    // upgrade nocodb-sdk version in nc-gui
-    execSync(`cd packages/nc-gui && npm install --save --save-exact ${nocodbSdkPackage.name}@${nocodbSdkPackage.version}`, {});
+  // upgrade nocodb-sdk version in nocodb
+  execSync(`cd packages/nocodb && npm install --save --save-exact ${nocodbSdkPackage.name}@${nocodbSdkPackage.version}`, {});
+  // upgrade nocodb-sdk version in nc-gui
+  execSync(`cd packages/nc-gui && npm install --save --save-exact ${nocodbSdkPackage.name}@${nocodbSdkPackage.version}`, {});
 }
 
-const dfs = function(dir) {
-    var res = [];
-    var list = fs.readdirSync(dir);
-    list.forEach(function(file) {
-        file = dir + '/' + file;
-        var stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) {
-            res = res.concat(dfs(file));
-        } else {
-            const ext = path.extname(file).toLowerCase()
-            if (ext == '.vue' || ext == '.ts') {
-                res.push(file);
-            }
-        }
-    })
-    return res;
+const dfs = function (dir) {
+  var res = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function (file) {
+    file = dir + '/' + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      res = res.concat(dfs(file));
+    } else {
+      const ext = path.extname(file).toLowerCase()
+      if (ext === '.vue' || ext === '.ts' || ext === 'js') {
+        res.push(file);
+      }
+    }
+  })
+  return res;
 }
 
 const searchAndReplace = (target) => {
-    const dirPath = path.resolve(path.join(__dirname, '..', 'packages'))
-    let list = dfs(dirPath)
-    list = [
-        ...list,
-        // path.join(__dirname, '..', 'packages', 'nc-gui', 'package.json'),
-        // path.join(__dirname, '..', 'packages', 'nocodb', 'package.json')
-    ]
-    return Promise.all(list.map(d => {
-        return new Promise((resolve, reject) => {
-            fs.readFile(d, function(err, content) {
-                if (err) reject(err)
-                if (content.indexOf(target) > -1) {
-                    replacePackageName(d).finally(()=>resolve())
-                }
+  const dirPath = path.resolve(path.join(__dirname, '..', 'packages'))
+  const list = dfs(dirPath)
 
-            })
-        })
-    }))
+  for(const pkg of ['nc-gui', 'nocodb']) {
+    const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'packages', pkg, 'package.json'), 'utf8'))
+    packageJson.dependencies[nocodbSdkPackage.name] = nocodbSdkPackage.version
+    fs.writeFileSync(path.join(__dirname, '..', 'packages', pkg, 'package.json'), JSON.stringify(packageJson, null, 2), 'utf8')
+  }
+
+  return Promise.all(list.map(d => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(d, function (err, content) {
+        if (err) reject(err)
+        if (content.indexOf(target) > -1) {
+          replacePackageName(d).finally(() => resolve())
+        }
+
+      })
+    })
+  }))
 }
 
 if (process.env.targetEnv === 'DEV') {
-    // replace nocodb-sdk by nocodb-sdk-daily if it is nightly build
-    searchAndReplace('nocodb-sdk')
+  // replace nocodb-sdk by nocodb-sdk-daily if it is nightly build
+  searchAndReplace('nocodb-sdk')
     .then(() => {
-        bumbVersionAndSave()
-        console.log(fs.readFileSync(path.join(__dirname, '..', 'packages', 'nc-gui', 'package.json'), 'utf8'))
+      bumbVersionAndSave()
+      console.log(fs.readFileSync(path.join(__dirname, '..', 'packages', 'nc-gui', 'package.json'), 'utf8'))
     })
 } else {
-    bumbVersionAndSave()
+  bumbVersionAndSave()
 }
